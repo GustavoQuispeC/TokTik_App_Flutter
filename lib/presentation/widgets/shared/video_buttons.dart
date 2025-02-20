1ad409d4d564d0784dcd933447c1af3cfba9a216
Gustavo <gusstavocta@gmail.com>
2025-02-19 23:06:02 -0500

    implementando video_player

--- lib/presentation/widgets/shared/video_buttons.dart
+++ lib/presentation/widgets/shared/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:toktik/config/helpers/human_formats.dart';
 import 'package:toktik/domain/entities/video_post.dart';
@@ -14,8 +15,18 @@ class VideoButtons extends StatelessWidget {
             value: video.likes,
             iconColor: Colors.red,
             iconData: Icons.favorite),
+        const SizedBox(height: 20),
         _CustomIconButton(
             value: video.views, iconData: Icons.remove_red_eye_outlined),
+        const SizedBox(height: 20),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 5),
+          child: const _CustomIconButton(
+            value: 0,
+            iconData: Icons.play_circle_outline,
+          ),
+        )
       ],
     );
   }
